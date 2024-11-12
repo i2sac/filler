e17af19a80a8562d65f358022af08a9d774a6375
lodiouf <louisisaacdf@gmail.com>
2024-11-12 17:59:22 +0000

    Optimization with parallelism thanks to  package

--- solution/src/lib.rs
+++ solution/src/lib.rs
@@ -1,7 +1,6 @@
 use std::cmp;
-use std::io::stdin;
-use std::io::BufRead;
-
+use std::io::{stdin, BufRead};
+use rayon::prelude::*;
 use rust_linalg::Matrix;
 
 #[derive(Debug, Clone)]
@@ -21,9 +20,8 @@ impl Game {
     }
 }
 
-/// Handler of all actions that occur during a turn
 pub fn turn(game: &mut Game) {
-    let input = stdin().lock().lines(); // Input reading
+    let input = stdin().lock().lines();
     let msg = "PARSING ERROR";
     let mut map_lines: Vec<String> = vec![];
     let mut piece_lines: Vec<String> = vec![];
@@ -31,13 +29,11 @@ pub fn turn(game: &mut Game) {
     let mut piece_recording = -1;
     let (mut piece_w, mut piece_h) = (0, 0);
 
-    // Lines processing loop
     for line in input {
         let txt = line.unwrap();
-
-        if let Some(p_cmd) = txt.strip_prefix("$$$ exec p") { // Get player number
+        if let Some(p_cmd) = txt.strip_prefix("$$$ exec p") {
             game.my_player = p_cmd.split_at(1).0.parse::<usize>().expect(&msg);
-        } else if let Some(size_raw) = txt.strip_prefix("Anfield ") { // Get map size
+        } else if let Some(size_raw) = txt.strip_prefix("Anfield ") {
             let size = size_raw
                 .replace(":", "")
                 .split(" ")
@@ -45,7 +41,7 @@ pub fn turn(game: &mut Game) {
                 .collect::<Vec<usize>>();
             (game.map_width, game.map_height) = (size[0], size[1]);
             map_recording = true;
-        } else if let Some(piece_size) = txt.strip_prefix("Piece ") { // Get piece size
+        } else if let Some(piece_size) = txt.strip_prefix("Piece ") {
             let size = piece_size
                 .replace(":", "")
                 .split(" ")
@@ -56,7 +52,6 @@ pub fn turn(game: &mut Game) {
             piece_recording = piece_h as i32 + 1;
         }
 
-        // Loop limiter
         if piece_recording > 0 {
             piece_lines.push(txt.clone());
             piece_recording -= 1;
@@ -66,7 +61,6 @@ pub fn turn(game: &mut Game) {
             map_lines.push(txt.clone().split_at(4).1.to_string());
         }
 
-        // Break condition for reading loop
         if piece_recording == 0 {
             break;
         }
@@ -77,7 +71,7 @@ pub fn turn(game: &mut Game) {
         .filter(|line| !line.starts_with("Piece "))
         .collect();
 
-    if map_lines.len() > 0 {
+    if !map_lines.is_empty() {
         let (player_chars, foe_chars) = match game.my_player {
             1 => (['@', 'a'], ['$', 's']),
             _ => (['$', 's'], ['@', 'a']),
@@ -85,7 +79,6 @@ pub fn turn(game: &mut Game) {
 
         let mut foe_pos: Vec<(usize, usize)> = vec![];
 
-        // Get foe's positions
         for (y, line) in map_lines.iter().enumerate() {
             for (x, c) in line.chars().enumerate() {
                 if foe_chars.contains(&c) {
@@ -94,10 +87,8 @@ pub fn turn(game: &mut Game) {
             }
         }
 
-        // Convolution limits
         let (mut min_x, mut max_x, mut min_y, mut max_y) = (game.map_width, 0, game.map_height, 0);
 
-        // Transform the map into a matrix
         let map_vecs = map_lines
             .iter()
             .enumerate()
@@ -106,74 +97,64 @@ pub fn turn(game: &mut Game) {
                     .enumerate()
                     .map(|(x, c)| {
                         if player_chars.contains(&c) {
-                            if x <= min_x {
-                                min_x = x;
-                            }
-                            if x >= max_x {
-                                max_x = x;
-                            }
-                            if y <= min_y {
-                                min_y = y;
-                            }
-                            if y >= max_y {
-                                max_y = y;
-                            }
-                            return 1;
+                            min_x = cmp::min(min_x, x);
+                            max_x = cmp::max(max_x, x);
+                            min_y = cmp::min(min_y, y);
+                            max_y = cmp::max(max_y, y);
+                            1
                         } else if foe_chars.contains(&c) {
-                            return 3;
+                            3
+                        } else {
+                            0
                         }
-                        0
                     })
                     .collect::<Vec<usize>>()
             })
             .collect::<Vec<Vec<usize>>>();
 
-        // Transform given piece into a matrix
         let piece_vecs = piece_lines
             .iter()
             .map(|line| {
                 line.chars()
-                    .map(|c| {
-                        if c == 'O' {
-                            return 1;
-                        }
-                        0
-                    })
+                    .map(|c| if c == 'O' { 1 } else { 0 })
                     .collect::<Vec<usize>>()
             })
             .collect::<Vec<Vec<usize>>>();
 
-        let mut pos_ok: Vec<(usize, usize)> = vec![];
-        let mut _mat: Matrix<usize> = Matrix::new(vec![vec![]]);
-
-        // Limit convolution to players most extreme pieces
         min_x = cmp::max(0, min_x as i32 - piece_w as i32) as usize;
         min_y = cmp::max(0, min_y as i32 - piece_h as i32) as usize;
 
-        // Get possible moves
-        for y in min_y..=max_y {
-            for x in min_x..=max_x {
-                if x + piece_w <= game.map_width && y + piece_h <= game.map_height {
-                    let piece_matrix = Matrix::new(piece_vecs.clone());
-                    let map_window = Matrix::new(
-                        map_vecs[y..(y + piece_h)]
-                            .into_iter()
-                            .map(|line| line[x..(x + piece_w)].to_vec())
-                            .collect(),
-                    );
-                    _mat = piece_matrix + map_window;
-                    let good_overlay = _mat.data.iter().flatten().filter(|v| **v == 2).count();
-                    let bad_overlay = _mat.data.iter().flatten().filter(|v| **v > 2).count();
-                    if good_overlay == 1 && bad_overlay == 0 {
-                        pos_ok.push((x, y));
-                    }
-                }
-            }
-        }
+        let pos_ok: Vec<(usize, usize)> = (min_y..=max_y)
+            .into_par_iter()
+            .flat_map(|y| {
+                (min_x..=max_x)
+                    .filter_map(|x| {
+                        if x + piece_w <= game.map_width && y + piece_h <= game.map_height {
+                            let piece_matrix = Matrix::new(piece_vecs.clone());
+                            let map_window = Matrix::new(
+                                map_vecs[y..(y + piece_h)]
+                                    .iter()
+                                    .map(|line| line[x..(x + piece_w)].to_vec())
+                                    .collect(),
+                            );
+                            let _mat = piece_matrix + map_window;
+                            let good_overlay = _mat.data.iter().flatten().filter(|&&v| v == 2).count();
+                            let bad_overlay = _mat.data.iter().flatten().filter(|&&v| v > 2).count();
+                            if good_overlay == 1 && bad_overlay == 0 {
+                                Some((x, y))
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect();
 
-        if pos_ok.len() > 0 {
+        if !pos_ok.is_empty() {
             let output_pos = closer_to_enemy(&pos_ok, &foe_pos);
-
             println!("{} {}", output_pos.0, output_pos.1);
         } else {
             println!("0 0");
@@ -181,10 +162,9 @@ pub fn turn(game: &mut Game) {
     }
 }
 
-/// Approaching the enemy and trying to block their progress
 fn closer_to_enemy(pos_ok: &[(usize, usize)], foe_pos: &[(usize, usize)]) -> (usize, usize) {
     pos_ok
-        .iter()
+        .par_iter()
         .min_by_key(|&&pos| {
             foe_pos
                 .iter()
@@ -195,4 +175,3 @@ fn closer_to_enemy(pos_ok: &[(usize, usize)], foe_pos: &[(usize, usize)]) -> (us
         .copied()
         .unwrap_or(pos_ok[0])
 }
-
